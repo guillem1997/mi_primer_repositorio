9ad4800512e53bea3c545b8e83a261d157d05b24
Guillem <guillemsimohospedales9@gmail.com>
2020-09-21 20:39:33 +0200

    Commit

--- guia/capitulo_6/cap6_app/views.py
+++ guia/capitulo_6/cap6_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psycopg2
 
 
 def home(request):
@@ -10,8 +11,13 @@ def anadir(request):
     prioridad = request.POST["name_prioridad"]
     titulo = request.POST["name_titulo"]
     nota = request.POST["name_nota"]
-    return (HttpResponse(f'Insertado <br> '
-                         f'prioridad: {prioridad}<br>'
-                         f'titulo: {titulo}<br>'
-                         f'nota: {nota}<br>'))
+    conn = psycopg2.connect(dbname="capitulo_6_db",
+                            user="capitulo_6_user",
+                            password="patata")
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO nota VALUES ('{prioridad}','{titulo}','{nota}');")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return render(request, 'home.html')
 # Create your views here.
