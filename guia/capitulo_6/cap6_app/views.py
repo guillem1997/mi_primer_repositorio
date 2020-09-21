e46553b7a36f1a21341bb9ea4bb1b75929d2b736
Guillem <guillemsimohospedales9@gmail.com>
2020-09-21 17:23:30 +0200

    Commit Capitulo_6.1

--- guia/capitulo_6/cap6_app/views.py
+++ guia/capitulo_6/cap6_app/views.py
@@ -4,6 +4,14 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+
 def anadir(request):
-    return render(request, 'anadir.html')
+    prioridad = request.POST["name_prioridad"]
+    titulo = request.POST["name_titulo"]
+    nota = request.POST["name_nota"]
+    return (HttpResponse(f'Insertado <br> '
+                         f'prioridad: {prioridad}<br>'
+                         f'titulo: {titulo}<br>'
+                         f'nota: {nota}<br>'))
 # Create your views here.
